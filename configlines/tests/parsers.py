06933423fc10f74ee2cac31848cd76f458ef279d
Benjamin Fogle <benfogle@gmail.com>
2018-05-06 15:20:57 -0400

    Testing other ConfigParser classes

--- configlines/tests/parsers.py
+++ configlines/tests/parsers.py
@@ -256,3 +256,46 @@ class ConfigTest(TestCase):
         self.assertEqual(cfg.get_location('qwerty', 'abc'), (path, 11))
         self.assertEqual(cfg.get_filename('qwerty', 'abc'), path)
         self.assertEqual(cfg.get_line('qwerty', 'abc'), 11)
+
+    def test_simple_raw(self):
+        cfg = configlines.RawConfigParser()
+        path = resource_filename(__name__, 'data1.cfg')
+        cfg.read(path)
+
+        self.assertEqual(cfg.get('foo', 'bar'), '1')
+        self.assertEqual(cfg.get_location('foo', 'bar'), (path, 2))
+        self.assertEqual(cfg.get_location('foo', 'BAR'), (path, 2))
+        self.assertEqual(cfg.get_filename('foo', 'bar'), path)
+        self.assertEqual(cfg.get_line('foo', 'bar'), 2)
+
+        self.assertEqual(cfg.get('foo', 'baz'), '2')
+        self.assertEqual(cfg.get_location('foo', 'baz'), (path, 3))
+        self.assertEqual(cfg.get_filename('foo', 'baz'), path)
+        self.assertEqual(cfg.get_line('foo', 'baz'), 3)
+
+        self.assertEqual(cfg.get('qwerty', 'abc'), 'a split\nline')
+        self.assertEqual(cfg.get_location('qwerty', 'abc'), (path, 11))
+        self.assertEqual(cfg.get_filename('qwerty', 'abc'), path)
+        self.assertEqual(cfg.get_line('qwerty', 'abc'), 11)
+
+    def test_simple_safe(self):
+        cfg = configlines.SafeConfigParser()
+        path = resource_filename(__name__, 'data1.cfg')
+        cfg.read(path)
+
+        self.assertEqual(cfg.get('foo', 'bar'), '1')
+        self.assertEqual(cfg.get_location('foo', 'bar'), (path, 2))
+        self.assertEqual(cfg.get_location('foo', 'BAR'), (path, 2))
+        self.assertEqual(cfg.get_filename('foo', 'bar'), path)
+        self.assertEqual(cfg.get_line('foo', 'bar'), 2)
+
+        self.assertEqual(cfg.get('foo', 'baz'), '2')
+        self.assertEqual(cfg.get_location('foo', 'baz'), (path, 3))
+        self.assertEqual(cfg.get_filename('foo', 'baz'), path)
+        self.assertEqual(cfg.get_line('foo', 'baz'), 3)
+
+        self.assertEqual(cfg.get('qwerty', 'abc'), 'a split\nline')
+        self.assertEqual(cfg.get_location('qwerty', 'abc'), (path, 11))
+        self.assertEqual(cfg.get_filename('qwerty', 'abc'), path)
+        self.assertEqual(cfg.get_line('qwerty', 'abc'), 11)
+
